d62a9eb0654abec5d95f119c5f6f836e090de172
Daud Abdiwahab <daudwahab1320@gmail.com>
2025-06-22 15:29:28 +0300

    done

--- server/app.py
+++ server/app.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response, jsonify, session
+from flask import Flask, jsonify, session
 from flask_migrate import Migrate
-
 from models import db, Article, User
 
 app = Flask(__name__)
@@ -12,23 +11,34 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
-
 db.init_app(app)
 
+# Clear session
 @app.route('/clear')
 def clear_session():
     session['page_views'] = 0
     return {'message': '200: Successfully cleared session data.'}, 200
 
+# Get all articles
 @app.route('/articles')
 def index_articles():
+    articles = Article.query.all()
+    return jsonify([article.to_dict() for article in articles]), 200
 
-    pass
-
+# Get single article with paywall logic
 @app.route('/articles/<int:id>')
 def show_article(id):
+    # Initialize page_views in session if not set
+    session['page_views'] = session.get('page_views', 0) + 1
 
-    pass
+    if session['page_views'] <= 3:
+        article = Article.query.get(id)
+        if article:
+            return jsonify(article.to_dict()), 200
+        else:
+            return {'message': 'Article not found'}, 404
+    else:
+        return {'message': 'Maximum pageview limit reached'}, 401
 
 if __name__ == '__main__':
     app.run(port=5555)
